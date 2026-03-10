670c2ac6795b0871444d3faf210c43a709f8f550
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-03-10 20:46:11 +0100

    Add input and button functionality

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,27 @@
 const btn = document.getElementById("hamburger-btn");
 const menu = document.getElementById("navbar-pages");
+const form = document.getElementById("form");
+const input = document.getElementById("search");
+const exploreBtn = document.getElementById("explore");
+const bookOneBtn = document.getElementById("book-one");
+const bookTwoBtn = document.getElementById("book-two");
+
+bookOneBtn.addEventListener("click", function () {
+  alert("You want to book now!");
+});
+
+bookTwoBtn.addEventListener("click", function () {
+  alert("You want to book now!");
+});
+
+exploreBtn.addEventListener("click", function () {
+  alert("You want to explore places!");
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert(`You have searched for "${input.value}"!`);
+});
 
 btn.addEventListener("click", function () {
   menu.classList.toggle("is-open");
